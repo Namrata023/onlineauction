c5682a0223bada0968a23e288bcdce6576e69cbe
S. Karki <sobitkarki7@gmail.com>
2025-01-09 20:52:33 +0545

    Change models.py

--- myapp/models.py
+++ myapp/models.py
@@ -1,15 +1,14 @@
 from django.db import models
 
 class User(models.Model):
-    id = models.IntegerField()
     name = models.CharField(max_length=100)
     email = models.CharField(max_length=100)
     password = models.CharField(max_length=100)
 
-class Items(models.Model):
-    user_id = models.ForeignKey(id, on_delete=models.CASCADE)
+class Item(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=100)
     minimum_price = models.FloatField()
-    image_path = models.ImageField(upload_to="/images")
+    image_path = models.ImageField(upload_to="images")
 
