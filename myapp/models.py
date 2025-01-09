a80c0f8ec201b4123804f2b756bac63b7bfe9909
Namrata Adhikari <namrata.adhikari35@gmail.com>
2025-01-09 21:52:02 +0545

    Updated

--- myapp/models.py
+++ myapp/models.py
@@ -5,10 +5,12 @@ class User(models.Model):
     email = models.CharField(max_length=100)
     password = models.CharField(max_length=100)
 
-class Items(models.Model):
-    user_id = models.ForeignKey(id, on_delete=models.CASCADE)
+class Item(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=100)
     minimum_price = models.FloatField()
     image_path = models.ImageField(upload_to="images")
+
+
 
