953bd77930ba5297fe1eaebceaf72a1fddeb8401
Namrata Adhikari <namrata.adhikari35@gmail.com>
2025-01-09 21:37:11 +0545

    updated

--- myapp/models.py
+++ myapp/models.py
@@ -7,7 +7,7 @@ class User(models.Model):
     password = models.CharField(max_length=100)
 
 class Items(models.Model):
-    user_id = models.ForeignKey(id, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(user, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=100)
     minimum_price = models.FloatField()
