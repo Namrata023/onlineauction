d3a11fc450caee975f851f978243c407bbb39423
S. Karki <sobitkarki7@gmail.com>
2025-01-10 11:20:06 +0545

    add a simple page for testing

---
+++ myapp/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('simple-page/', views.simple_page_view, name='simple_page'),
+]
--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
+def simple_page_view(request):
+    return render(request, 'myapp/simple_page.html')
 
 
 
