ea1b1bf38771315c9db78425a98ebc664e61ecdc
Namrata Adhikari <namrata.adhikari35@gmail.com>
2025-01-10 21:17:54 +0545

    updated models

--- myapp/models.py
+++ myapp/models.py
@@ -12,5 +12,19 @@ class Item(models.Model):
     minimum_price = models.FloatField()
     image_path = models.ImageField(upload_to="images")
 
+class Feedback(models.Model):
+    name = models.CharField(max_length=100)
+    message = models.CharField(max_length=1000)
+    email = models.CharField(max_length=100)
+ 
+class Bid(models.Model):
+    item_id = models.ForeignKey(Item, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    bid_price = models.FloatField()
+
+
+    
+  
+
 
 
