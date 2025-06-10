be6be185b4b854c855af86eae78a21a32206af02
Namrata023 <namrata.adhikari35@gmail.com>
2025-06-10 21:10:30 +0545

    updated: endtime

--- myapp/forms.py
+++ myapp/forms.py
@@ -1,11 +1,18 @@
 from django import forms
 from .models import Item, Bid, Feedback, CustomUser
 from django.contrib.auth.forms import UserCreationForm as BaseUserCreationForm
+from django.core.exceptions import ValidationError
+from django.utils import timezone
+
 
 class ItemForm(forms.ModelForm):
     class Meta:
         model = Item
-        fields = ['name', 'description', 'minimum_price', 'image']
+        fields = ['name', 'description', 'minimum_price','end_time', 'image']
+        widgets = {
+            'end_time': forms.DateTimeInput(attrs={'type': 'datetime-local'}),
+        }
+        
 
 class BidForm(forms.ModelForm):
     class Meta:
