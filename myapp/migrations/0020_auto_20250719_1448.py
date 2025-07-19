088514ba29940351f625f8045e8808abed0a5526
Namrata023 <namrata.adhikari35@gmail.com>
2025-07-19 15:30:45 +0545

    solve auto-add-items errors

--- myapp/migrations/0020_auto_20250719_1448.py
+++ myapp/migrations/0020_auto_20250719_1448.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('myapp', '0019_alter_item_end_time'),
+        ('myapp', '0001_initial'),
     ]
 
     operations = [
