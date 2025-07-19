3ef61a2e1ecc0a68442a63a40d9bb9811a038a75
Namrata023 <namrata.adhikari35@gmail.com>
2025-07-20 01:16:24 +0545

    update:models.py

--- myapp/models.py
+++ myapp/models.py
@@ -53,7 +53,7 @@ class Item(models.Model):
     
     owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
-    category = models.CharField(max_length=100, choices=CATEGORY_CHOICES, default='electronics')
+    category = models.CharField(max_length=100, choices=CATEGORY_CHOICES, default='AllCategories')
     description = models.CharField(max_length=10000)
     tags = models.CharField(max_length=255, blank=True, null=True)
     minimum_price = models.FloatField()
